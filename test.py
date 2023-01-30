2a89670194fb1cde84edbd3f0233c8df90891a83
nigero <maartendormenatteysen@hotmail.com>
2023-01-30 21:03:28 +0100

    add the svix connection

--- test.py
+++ test.py
@@ -2,9 +2,7 @@ import requests
 
 url = 'https://play.svix.com/view/c_Q0l1PbA3m6RbYHyiBlaJEVhW0Sl/'
 
-payload = {"ticker": "BTCUSDT" ,
-"side" : 1,
-"time": "30m"}
+payload = {"ticker": "BTCUSDT" ,"side" : 1,"time": "30m"}
 
 
 
