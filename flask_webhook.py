cd94425c4c7d15c4edf5638a0ac0241df4f330f8
nigero <maartendormenatteysen@hotmail.com>
2023-03-27 22:23:23 +0200

    added suprocess for svinx into the mix

--- flask_webhook.py
+++ flask_webhook.py
@@ -1,10 +1,28 @@
 import queue
 import threading
+import subprocess
+import time
+import atexit 
 
 from flask import Flask, redirect, request, url_for
 
 from trading import TradeCrypto
 from utils.exchanges import get_exchanges
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def print_date_time():
+    p = subprocess.Popen("svix listen http://localhost:8000/webhook/", stdout=subprocess.PIPE, shell=True)
+    print(p.communicate())
+
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=print_date_time, trigger="interval", seconds=7_200)
+scheduler.start()
+
+# Shut down the scheduler when exiting the app
+atexit.register(scheduler.shutdown)
 
 thread = None
 app = Flask(__name__)
@@ -17,9 +35,12 @@ def hook():
     
     incoming_data = request.get_json()
     incoming_data_queue.put(incoming_data)
+
     
      # Redirect to the start_processing endpoint
     return redirect(url_for('start_processing'))
+
+
 
 def process_data():
     exchanges: dict[str, any] = get_exchanges()
