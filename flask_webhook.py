dd69d7a2af8947e5481ad91674482e29a58af3d5
nigero <maartendormenatteysen@hotmail.com>
2023-03-29 00:03:37 +0200

    chatpgpt extra

--- flask_webhook.py
+++ flask_webhook.py
@@ -1,77 +1,84 @@
-import atexit
+import logging
 import queue
+import signal
 import subprocess
 import threading
-import time
 
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask, redirect, request, url_for
+from flask import Flask, jsonify, request
 
 from trading import TradeCrypto
 from utils.exchanges import get_exchanges
 
-def run_svinx() -> None:
-    try:
-        p = subprocess.Popen("svix listen http://localhost:8000/webhook/", stdout=subprocess.PIPE, shell=True)
-        print("running the command 'svix listen http://localhost:8000/webhook/' ")
-    except subprocess.CalledProcessError as e:
-        print(e)
-    except Exception as e:
-        print(e)
-
-thread = None
 app = Flask(__name__)
 
 # Create a thread-safe queue to store incoming data
 incoming_data_queue = queue.Queue()
 
-@app.route('/webhook/', methods=['POST'])
-def hook():
-    
-    incoming_data = request.get_json()
-    
+# Create an event to signal the thread to stop
+stop_event = threading.Event()
+
+def run_svinx():
     try:
-        incoming_data_queue.put(incoming_data)
+        p = subprocess.Popen("svix listen http://localhost:8000/webhook/", stdout=subprocess.PIPE, shell=True)
+        logging.info("Running the command 'svix listen http://localhost:8000/webhook/'")
+    except subprocess.CalledProcessError as e:
+        logging.error(e)
     except Exception as e:
-        print(e)
-
-    
-     # Redirect to the start_processing endpoint
-    return redirect(url_for('start_processing'))
-
-
+        logging.error(e)
 
 def process_data():
-    exchanges: dict[str, any] = get_exchanges()
-    
-    while True:
+    exchanges = get_exchanges()
+    while not stop_event.is_set():
         try:
             incoming_data = incoming_data_queue.get(timeout=1)
             TradeCrypto(incoming_data, exchanges)
         except queue.Empty:
-            break
-        
-    return 'Data processed'
+            pass
+        except Exception as e:
+            logging.error(e)
+    logging.info("Thread stopped")
 
+@app.route('/webhook/', methods=['POST'])
+def hook():
+    incoming_data = request.get_json()
+    try:
+        incoming_data_queue.put(incoming_data)
+    except Exception as e:
+        logging.error(e)
+        logging.exception(e)
+    return jsonify(success=True)
 
-@app.route('/process/')
-def start_processing():
-    # Start a separate thread to process incoming data
-    global thread
-    if thread is None or not thread.is_alive():
-        thread = threading.Thread(target=process_data)
-        thread.start()
-        
-    return 'Data processing started'
+def handle_signal(signal_number, frame):
+    logging.info(f"Received signal {signal_number}. Shutting down...")
+    stop_event.set()
+    request.environ.get('werkzeug.server.shutdown')()
 
 if __name__ == '__main__':
+    # Set up logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    # Start the thread
+    thread = threading.Thread(target=process_data)
+    thread.start()
+
+    # Start the svix process
     run_svinx()
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=run_svinx, trigger="interval", seconds=7_200)
+    scheduler.add_job(func=run_svinx, trigger="interval", seconds=7200)
     scheduler.start()
 
-    # Shut down the scheduler when exiting the app
-    atexit.register(scheduler.shutdown)
-    
-    #run the flask app
-    app.run(host='127.0.0.1', port=8000)
+    # Register signal handlers
+    signal.signal(signal.SIGINT, handle_signal)
+    signal.signal(signal.SIGTERM, handle_signal)
+
+    # Start the Flask app
+    with app.app_context():
+        app.run(host='127.0.0.1', port=8000)
+
+    # Signal the thread to stop and wait for it to finish
+    stop_event.set()
+    thread.join()
+
+    # Stop the scheduler
+    scheduler.shutdown()
